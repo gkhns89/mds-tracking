e8d91237a4a9900cb4b7193765319702909e2f04
Gökhan <gokhansisman@outlook.com>
2025-03-11 03:16:09 +0300

    Fire Medosa Tracking!

---
+++ src/test/java/com/medosasoftware/mdstracking/MdsTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medosasoftware.mdstracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdsTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
