1741a194d908354e220c90ce0d1680eb1021e771
Gökhan <gokhansisman@outlook.com>
2025-03-12 09:27:54 +0300

    Fix for service files

--- src/main/java/com/medosasoftware/mdstracking/service/UserService.java
+++ src/main/java/com/medosasoftware/mdstracking/service/UserService.java
@@ -6,12 +6,20 @@ import com.medosasoftware.mdstracking.model.User;
 import com.medosasoftware.mdstracking.repository.CompanyRepository;
 import com.medosasoftware.mdstracking.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+
     @Autowired
     private UserRepository userRepository;
 
@@ -36,8 +44,26 @@ public class UserService {
         return user.getRole() == Role.ADMIN;
     }
 
-
     public Optional<User> findByEmail(String email) {
         return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = findByEmail(username);
+        if (!user.isPresent()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new org.springframework.security.core.userdetails.User(user.get().getEmail(), user.get().getPassword(), getAuthorities(user.get().getRole()));
+    }
+
+    private List<GrantedAuthority> getAuthorities(Role role) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        if (role == Role.ADMIN) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        } else {
+            authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+        return authorities;
     }
 }
