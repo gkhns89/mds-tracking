b65db7d70bc03614b1d92b3af1836e0b194dc6f5
Gökhan <gokhansisman@outlook.com>
2025-03-12 09:27:54 +0300

    Fix for service files

--- src/main/java/com/medosasoftware/mdstracking/service/AuthService.java
+++ src/main/java/com/medosasoftware/mdstracking/service/AuthService.java
@@ -5,7 +5,6 @@ import com.medosasoftware.mdstracking.dto.AuthResponse;
 import com.medosasoftware.mdstracking.dto.RegisterRequest;
 import com.medosasoftware.mdstracking.model.Role;
 import com.medosasoftware.mdstracking.model.User;
-import com.medosasoftware.mdstracking.repository.UserRepository;
 import com.medosasoftware.mdstracking.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,7 +13,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-    private final UserRepository userRepository;
+    private final UserService userService;
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
 
@@ -25,14 +24,14 @@ public class AuthService {
         user.setPassword(passwordEncoder.encode(request.getPassword()));
         user.setRole(Role.USER); // Varsayılan olarak USER rolü atanıyor
 
-        userRepository.save(user);
+        userService.createUser(user);
         String token = jwtTokenProvider.generateToken(user.getEmail());
 
         return new AuthResponse(token);
     }
 
     public AuthResponse authenticate(AuthRequest request) {
-        User user = userRepository.findByEmail(request.getEmail())
+        User user = userService.findByEmail(request.getEmail())
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
         if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
